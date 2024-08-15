b2f031ecf8199ff5f4787baa078867e99008ff99
Andrew Furman <aifurman@gmail.com>
2024-08-15 10:31:19 +0000

    reverted still working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from pdf_processor import extract_text_from_pdf_url
+from pdf_processor import get_url_text
 
 app = Flask(__name__)
 
@@ -9,9 +9,10 @@ def index():
 
 @app.route('/process_pdf', methods=['POST'])
 def process_pdf():
-    pdf_url = request.json['pdf_url']
-    extracted_text = extract_text_from_pdf_url(pdf_url)
-    return jsonify({'text': extracted_text})
+    data = request.json
+    pdf_url = data.get('pdf_url')
+    text = get_url_text(pdf_url)
+    return jsonify({'text': text})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
